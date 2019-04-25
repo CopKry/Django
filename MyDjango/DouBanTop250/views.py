6a51f5115f67649e54feb8c1bc657685499bb53f
CopKry <49380171+CopKry@users.noreply.github.com>
2019-04-25 13:48:45 +0800

    爬虫数据与分析期中作业

---
+++ MyDjango/DouBanTop250/views.py
@@ -0,0 +1,6 @@
+from django.shortcuts import render
+from DouBanTop250.models import douban
+
+def douban_dir(request):
+    rows=douban.objects.all()
+    return render(request,'Top250.html',{'content':rows})
